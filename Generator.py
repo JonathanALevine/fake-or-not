ea9c8f3625f1971764a8515af51e108589036c15
Jonathan Levine <jonathanaaron.levine@gmail.com>
2023-03-30 16:48:50 -0400

    making the generator

--- Generator.py
+++ Generator.py
@@ -4,6 +4,18 @@ from torch import nn
 torch.manual_seed(18)
 torch.cuda.is_available()
 from datetime import datetime
+from PIL import Image
+from torchvision import transforms
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+
+    def forward(self, x):
+        pass
 
 
 def main():
@@ -12,3 +24,8 @@ def main():
 
 if __name__ == '__main__':
     main()
+    test_tensor = torch.rand(3, 256, 256)
+    print(test_tensor)
+    image = transforms.ToPILImage()(test_tensor)
+    image.show()
+    
