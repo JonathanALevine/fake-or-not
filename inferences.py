6e5adcdc26542d198fc2c8f0ddc122c65768ba84
jonathan <jonathanaaron.levine@gmail.com>
2024-03-07 21:31:23 -0500

    better file name

--- inferences.py
+++ inferences.py
@@ -53,8 +53,6 @@ if __name__=="__main__":
     print(torch.cuda.is_available())
     tt0 = TTDevice("grayskull0")
     discriminator = pybuda.PyTorchModule('DiscriminatorV4', DiscriminatorV4())
-    # weight = torch.rand(*act_dim).half()
-    # discriminator.set_parameter("weights",  weight)
     tt0.place_module(discriminator)
 
     act = torch.randn(1, 3, 128, 128)
@@ -65,20 +63,3 @@ if __name__=="__main__":
     val = result.get()[0]
     print(val.value())
     print(val.shape)
-
-    # Add a CPU device to calculate loss
-    params = discriminator.parameters()
-    learning_rate = 3e-4
-    optimizer = torch.optim.Adam(params, lr=learning_rate)
-    # cpu = CPUDevice("cpu0", optimizer_f = None, scheduler_f = None)
-    cpu = CPUDevice("cpu0", optimizer_f=optimizer, scheduler_f=None)
-    # declare the loss function
-    loss_module = PyTorchModule("bceloss", nn.BCELoss())
-    cpu.place_loss_module(loss_module)
-
-    target = torch.tensor([1])
-    cpu.push_to_target_inputs(target)
-    print(target)
-
-    tt0.push_to_inputs(act)
-    pybuda.run_training(epochs=1, steps=1, accumulation_steps=1, microbatch_count=1)
